38a1611a1e9498f6227d9778345330806233b4cc
Sebastian Garcia <js.garcial1@uniandes.edu.co>
2020-01-28 11:33:08 -0500

    Initial Commit

---
+++ Bluetooth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bluetooth
+//
+//  Created by Sebastian on 1/28/20.
+//  Copyright © 2020 Sebastian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
